e836925d8b7716f181cbbdd3ebd6ae2e5e4c67ab
cal <m7od99@icloud.com>
2021-01-20 22:02:42 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace from18to20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
